18623db87a3e9fe0d3e001136f3eb220382acce2
Eduard Lehmann <Eduard.Lehmann@tlc-software.de>
2017-08-22 12:02:35 +0100

    info A1 class

--- av64/src/av64/Av10064.java
+++ av64/src/av64/Av10064.java
@@ -10,12 +10,12 @@ package av64;
 public class Av10064 {
 
 	/**
-	 * v64 main -1
+	 * v64 main -1 
 	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+		//user2: A1
 			
 	}
 
