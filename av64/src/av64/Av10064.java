510941689138fb7f6789448a1bb54df5761acf0a
Heiner Hempel <heiner.hempel@tlc-software.de>
2017-08-22 09:22:30 +0100

    initial commit

---
+++ av64/src/av64/Av10064.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package av64;
+
+/**
+ * @author 
+ *
+ */
+public class Av10064 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ av64/src/av64/Bv10066.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package av64;
+
+/**
+ * @author admin
+ *
+ */
+public class Bv10066 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
